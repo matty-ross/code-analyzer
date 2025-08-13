2a7c2d5e34ced42dcf236e08d926e0a8fc8b2d02
Matty <54020396+matty-ross@users.noreply.github.com>
2025-08-13 19:45:12 +0200

    Refactored and added config.

--- code-analyzer/src/CodeAnalyzer.cpp
+++ code-analyzer/src/CodeAnalyzer.cpp
@@ -1,6 +1,10 @@
 #include "CodeAnalyzer.hpp"
 
 
+static constexpr char k_ConfigFileName[] = ".\\config.ini";
+static constexpr char k_SingleStepFileName[] = ".\\output\\single_step.bin";
+
+
 CodeAnalyzer CodeAnalyzer::s_Instance;
 
 
@@ -11,32 +15,93 @@ CodeAnalyzer& CodeAnalyzer::Get()
 
 void CodeAnalyzer::OnProcessAttach()
 {
-    SetUnhandledExceptionFilter(
-        [](EXCEPTION_POINTERS* ExceptionInfo) -> LONG
-        {
-            s_Instance.OnException(ExceptionInfo);
-            return EXCEPTION_CONTINUE_EXECUTION;
-        }
-    );
+    LoadConfig();
+
+    GetModuleInformation(GetCurrentProcess(), GetModuleHandleA(nullptr), &m_ModuleInfo, sizeof(m_ModuleInfo));
+    SetUnhandledExceptionFilter(&CodeAnalyzer::TopLevelExceptionFilter);
+
+    if (m_Config.SingleStep)
+    {
+        fopen_s(&m_SingleStepFile, k_SingleStepFileName, "wb");
+    }
 }
 
 void CodeAnalyzer::OnProcessDetach()
 {
+    if (m_Config.SingleStep)
+    {
+        fclose(m_SingleStepFile);
+    }
+    
     MessageBoxA(NULL, "Finished code analysis.", "Code Analyzer", MB_OK);
 }
 
-void CodeAnalyzer::OnException(EXCEPTION_POINTERS* exceptionInfo)
+void CodeAnalyzer::LoadConfig()
 {
-    DWORD exceptionCode = exceptionInfo->ExceptionRecord->ExceptionCode;
+    static constexpr char section[] = "Config";
     
-    switch (exceptionCode)
+    m_Config.SingleStep = GetPrivateProfileIntA(section, "SingleStep", 0, k_ConfigFileName);
+}
+
+void CodeAnalyzer::OnExceptionAccessViolation(EXCEPTION_POINTERS* exceptionInfo)
+{
+}
+
+void CodeAnalyzer::OnExceptionSingleStep(EXCEPTION_POINTERS* exceptionInfo)
+{
+    DWORD eip = exceptionInfo->ContextRecord->Eip;
+    if (m_Config.SingleStep)
+    {
+        fwrite(&eip, sizeof(eip), 1, m_SingleStepFile);
+    }
+    
+    DisableSingleStepping(exceptionInfo);
+    if (m_Config.SingleStep)
+    {
+        SetGuardPage();
+    }
+}
+
+void CodeAnalyzer::OnExceptionGuardPage(EXCEPTION_POINTERS* exceptionInfo)
+{
+    if (m_Config.SingleStep)
+    {
+        EnableSingleStepping(exceptionInfo);
+    }
+}
+
+void CodeAnalyzer::EnableSingleStepping(EXCEPTION_POINTERS* exceptionInfo)
+{
+    exceptionInfo->ContextRecord->EFlags |= 0x100;
+}
+
+void CodeAnalyzer::DisableSingleStepping(EXCEPTION_POINTERS* exceptionInfo)
+{
+    exceptionInfo->ContextRecord->EFlags &= ~0x100;
+}
+
+void CodeAnalyzer::SetGuardPage()
+{
+    DWORD oldProtection = 0;
+    VirtualProtect(m_ModuleInfo.lpBaseOfDll, m_ModuleInfo.SizeOfImage, PAGE_EXECUTE_READWRITE | PAGE_GUARD, &oldProtection);
+}
+
+LONG CALLBACK CodeAnalyzer::TopLevelExceptionFilter(EXCEPTION_POINTERS* ExceptionInfo)
+{
+    switch (ExceptionInfo->ExceptionRecord->ExceptionCode)
     {
     case EXCEPTION_ACCESS_VIOLATION:
-        m_AnalyzerSingleStep.OnExceptionAccessViolation(exceptionInfo);
+        s_Instance.OnExceptionAccessViolation(ExceptionInfo);
         break;
-    
+
     case EXCEPTION_SINGLE_STEP:
-        m_AnalyzerSingleStep.OnExceptionSingleStep(exceptionInfo);
+        s_Instance.OnExceptionSingleStep(ExceptionInfo);
+        break;
+
+    case EXCEPTION_GUARD_PAGE:
+        s_Instance.OnExceptionGuardPage(ExceptionInfo);
         break;
     }
+    
+    return EXCEPTION_CONTINUE_EXECUTION;
 }
--- code-analyzer/src/CodeAnalyzer.hpp
+++ code-analyzer/src/CodeAnalyzer.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
 
+#include <cstdio>
 #include <Windows.h>
+#include <Psapi.h>
 
-#include "Analyzer/SingleStep.hpp"
+
+struct Config
+{
+    bool SingleStep = false;
+};
 
 
 class CodeAnalyzer
@@ -16,11 +22,27 @@ public:
     void OnProcessDetach();
 
 private:
-    void OnException(EXCEPTION_POINTERS* exceptionInfo);
+    void LoadConfig();
+
+    void OnExceptionAccessViolation(EXCEPTION_POINTERS* exceptionInfo);
+    void OnExceptionSingleStep(EXCEPTION_POINTERS* exceptionInfo);
+    void OnExceptionGuardPage(EXCEPTION_POINTERS* exceptionInfo);
+
+    void EnableSingleStepping(EXCEPTION_POINTERS* exceptionInfo);
+    void DisableSingleStepping(EXCEPTION_POINTERS* exceptionInfo);
+
+    void SetGuardPage();
+
+private:
+    static LONG CALLBACK TopLevelExceptionFilter(EXCEPTION_POINTERS* ExceptionInfo);
 
 private:
     static CodeAnalyzer s_Instance;
 
 private:
-    Analyzer::SingleStep m_AnalyzerSingleStep;
+    Config m_Config = {};
+    
+    MODULEINFO m_ModuleInfo = {};
+
+    FILE* m_SingleStepFile = nullptr;
 };
